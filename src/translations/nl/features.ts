c66a9c2a1162d1bde9112b70e33e484e1ffe0d77
24dv <david.g.vale@gmail.com>
2025-05-28 11:48:42 +0200

    Update features.ts

--- src/translations/nl/features.ts
+++ src/translations/nl/features.ts
@@ -4,7 +4,7 @@ const features = {
   "features.subtitle": "Of je nu een founder bent met een gedurfd idee, een freelancer die wil doorgroeien, of een side hustle wil opstarten — dit is iets voor jou. Met ons Launchpakket ga je snel én zonder stress live.",
   "features.logoDesign.title": "Professioneel logo en kleuren",
   "features.logoDesign.description": "Val op met een logo en kleuren die je merkidentiteit perfect weerspiegelen.",
-  "features.landingSite.title": "Gepersonaliseerde single page website",
+  "features.landingSite.title": "Gepersonaliseerde website",
   "features.landingSite.description": "Een stijlvolle, responsive website die bezoekers omzet in klanten.",
   "features.delivery.title": "Snelle oplevering binnen 14 dagen",
   "features.delivery.description": "Start snel op, zonder in te boeten op kwaliteit.",
