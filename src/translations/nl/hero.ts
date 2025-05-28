48a1ecddb6f916b94f20a8e4c314bc0cc3a4eb72
24dv <david.g.vale@gmail.com>
2025-05-28 11:43:24 +0200

    Update hero.ts

--- src/translations/nl/hero.ts
+++ src/translations/nl/hero.ts
@@ -3,7 +3,7 @@ const hero = {
   "hero.badge": "Nieuw",
   "hero.badgeText": "Lanceer je merk in dagen, niet maanden",
   "hero.subtitle": "Op maat gemaakte branding en website voor €1500—stressvrij in 14 dagen!",
-  "hero.cta": "Plan Je Gratis Introgesprek",
+  "hero.cta": "Boek Je Gratis Kennismakingscall",
   "hero.scroll": "Begin met slechts een paar klikken",
 };
 
